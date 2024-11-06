9d52db4eedbb46a8cd3252f1a26b57a2b99e218b
sunkyung96 <deobeulyu62@gmail.com>
2024-11-06 10:50:42 +0900

    인벤토리

---
+++ Assets/Scenes/ItemDetector.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+    //아이템 종류 정의
+    public enum ItemType
+    {
+        Crystal, //크리스탈
+        Plant, //식물
+        Bush, //수풀
+        Tree,//나무
+    }
+
+public class ItemDetector : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+    //Update is called once per frame
+    
+
+    }
+
+
+
--- Assets/Scenes/PlayeAnimatiomManager.cs
+++ Assets/Scenes/PlayeAnimatiomManager.cs
@@ -4,15 +4,55 @@ using UnityEngine;
 
 public class PlayeAnimatiomManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Animator animator; //애니메이션을 관리하는 오브젝트
+    public PlayeAnimatiomManager stateMachine; //사용자가 정리한 상태 정의
+
+    //애니메이터 파라미터 이름들을 상수로 정의
+    private const string PARAM_IS_MOVING = "IsMoving";
+    private const string PARAM_IS_RUNNING = "IsRunning";
+    private const string PARAM_IS_JUMPING = "IsJumping";
+    private const string RARAM_IS_FALLING = "IsFalling";
+    private const string PARAM_ATTACK_TRIGGER= "Attack";
+
 
     // Update is called once per frame
     void Update()
     {
         
     }
+    private void UpdateAnimationState()
+    {
+        //현재 상태에 따라 애니메이션 파라미터 설정
+        if(stateMachine.currentState !=null)
+        {
+            //모든 bool 파라미터를 초기화
+            ResetAIIBoolParameters();
+
+            //현재 상태에 따라 해당하는 애니메이션 파라미터를 설정
+            switch (stateMachine.currentState)
+            {
+                case IdleState;
+                    //Idle 상태는 모른 파라미터가 false인 상태
+                    break;
+                    case MovingState;
+                    {
+
+                    }
+
+                    
+            }
+        }
+    }
+    private void TriggerAttack()
+    {
+        animator.SetTrigger(PARAM_ATTACK_TRIGGER);
+    }
+    private void ResetAIIBoolParameters()
+    {
+        animator.SetBool(PARAM_IS_MOVING, false);
+        animator.SetBool(PARAM_IS_RUNNING, false);
+        animator.SetBool(PARAM_IS_JUMPING, false);
+        animator.SetBool(RARAM_IS_FALLING, false);
+    }
+    
 }
